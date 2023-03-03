234ba3710767c3f8cf839af2a96d2e8f98cf3e40
Dani <zsquiid@gmail.com>
2023-03-03 14:58:49 +0100

    fikset stor "H" i header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <Header>
+    <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Danial Ikram</h1>
         <h5 className="text-light">Fullstack Developer</h5>
       </div>
-    </Header>
+    </header>
   )
 }
 
