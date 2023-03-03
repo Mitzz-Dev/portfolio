a8eca6135a3282076296e5e126db65fe7e37d21c
Dani <zsquiid@gmail.com>
2023-03-03 15:12:08 +0100

    Endring I Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -8,7 +10,15 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Danial Ikram</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
       </div>
+
+      <div className="me">
+        <img src={ME} alt="" />
+      </div>
+
+      <a href='#contact' className='scroll_down'>Scroll Down</a>
+
     </header>
   )
 }
